b79ae0984078de007db7ee2803955fb7c681703f
Nuraasyikin <22FTT1504@student.pb.edu.bn>
2024-03-11 10:20:02 +0800

    I import the IonMenu, etc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonContent, IonHeader, IonToolbar, IonTitle, IonList, IonLabel, IonMenuToggle } from '@ionic/angular/standalone';
+import { IonMenu } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonLabel, IonList, IonTitle, IonToolbar, IonHeader, IonContent, IonApp, IonRouterOutlet, IonMenu, IonMenuToggle],
 })
 export class AppComponent {
   constructor() {}
