386ff4dc02488e22232e44df2d00d7cbd58d0282
Nuraasyikin <22FTT1504@student.pb.edu.bn>
2024-03-24 17:01:55 +0800

    Modify the auth page

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -12,10 +14,21 @@ import { RouterModule } from '@angular/router';
   imports: [IonicModule, CommonModule, FormsModule, RouterModule]
 })
 export class AuthPage implements OnInit {
+email: any;
+password: any;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  async login(email: string, password: string): Promise<void> {
+    try {
+      await this.authService.login(email, password);
+      // Redirect to places page upon successful login
+      this.router.navigate(['/places']);
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
   }
 
 }
